2c83047c4c3288b69c85d9b9c0bdef80d6962990
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-11-10 12:27:21 -0700

    useContext: la fusio de react hooks y react context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import ThemeContext from '../context/ThemeContext';
 
 const Header = () => {
 	const [darkmode, setDarkmode] = useState(false);
+	const color = useContext(ThemeContext);
 
 	const handleClick = () => {
 		setDarkmode(!darkmode);
 	}
 	return (
 		<div className="Header">
-			<h1>React Hooks</h1>
+			<h1 style={{ color }}>React Hooks</h1>
 			<button 
 			type="button" 
 			onClick={() => handleClick()}
@@ -21,7 +23,7 @@ const Header = () => {
 			>
 				Change theme 2
 			</button>
-			<h2>{darkmode ? 'Dark mode Active' : 'Light mode'}</h2>
+			<h3>{darkmode ? 'Dark mode Active' : 'Light mode'}</h3>
 		</div>
 	);
 }
