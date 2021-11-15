be5f1a37f93a0228b88cbc0f25cfbb585dad1f9b
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-11-15 11:15:58 -0700

    maquetacion y estilos del flujo de pago

--- platzi-conf-store/src/containers/Success.jsx
+++ platzi-conf-store/src/containers/Success.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import '../styles/components/Success.css'
 
 const Success = () => {
-  return <h1>Success</h1>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Gracias Eru, por tu compra</h2>
+        <span>Tu pedido llegara de 3 a 5 dias habiles a tu direccion:</span>
+        <div className="Success-map">
+          Google Maps
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
