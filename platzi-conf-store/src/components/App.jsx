2b3662c22533ed9e77bd64d090947de6aca7785a
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-11-15 10:09:16 -0700

    maquetacion y estilos del home

--- platzi-conf-store/src/components/App.jsx
+++ platzi-conf-store/src/components/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 const hola = 'hola';
 
-import '../styles/components/app.css';
-
 const App = () => <h1>hola mundo</h1>;
 
 export default App;
--- platzi-conf-store/src/routes/App.jsx
+++ platzi-conf-store/src/routes/App.jsx
@@ -6,18 +6,21 @@ import Information from '../containers/Information';
 import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
+import Layout from '../components/Layout';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/checkout/information" element={<Information />} />
-        <Route path="/checkout/payment" element={<Payment />} />
-        <Route path="/checkout/success" element={<Success />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/information" element={<Information />} />
+          <Route path="/checkout/payment" element={<Payment />} />
+          <Route path="/checkout/success" element={<Success />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
