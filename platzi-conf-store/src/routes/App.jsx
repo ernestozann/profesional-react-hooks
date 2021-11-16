ea657f47bd16eedf5d967efadda2ad92e99c3a04
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-11-16 12:48:49 -0700

    creando nuestro primer custom hook

--- platzi-conf-store/src/routes/App.jsx
+++ platzi-conf-store/src/routes/App.jsx
@@ -7,21 +7,25 @@ import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
 import Layout from '../components/Layout';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/checkout/information" element={<Information />} />
-          <Route path="/checkout/payment" element={<Payment />} />
-          <Route path="/checkout/success" element={<Success />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/checkout/information" element={<Information />} />
+            <Route path="/checkout/payment" element={<Payment />} />
+            <Route path="/checkout/success" element={<Success />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
