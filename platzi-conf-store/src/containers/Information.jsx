7310e33f8f833ced06f04d69cbbe422a173b29f6
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-11-17 12:33:57 -0700

    useRef en la pagina de checkout

--- platzi-conf-store/src/containers/Information.jsx
+++ platzi-conf-store/src/containers/Information.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useRef, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import '../styles/components/Information.css';
 
 const Information = () => {
+  const { state,addToBuyer } = useContext(AppContext);
+  const form = useRef(null);
+
+  const { cart } = state;
+
+  const handleSubmit = () => {
+    const formData = new FormData(form.current);
+    const buyer = {
+      'name': formData.get('name'),
+      'email': formData.get('email'),
+      'address': formData.get('address'),
+      'apto': formData.get('apto'),
+      'city': formData.get('city'),
+      'country': formData.get('country'),
+      'state': formData.get('state'),
+      'cp': formData.get('cp'),
+      'phone': formData.get('phone'),
+    }
+    addToBuyer(buyer);
+  }
+
   return (
     <div className="Information">
       <div className="Infomation-content">
@@ -10,7 +32,7 @@ const Information = () => {
           <h2>Informacion de contacto:</h2>
         </div>
         <div className="Infomation-form">
-          <form action="">
+          <form ref={form}>
             <input type="text" placeholder="Full name" name='name' />
             <input type="text" placeholder="Email" name='email' />
             <input type="text" placeholder="Address" name='address' />
@@ -29,20 +51,20 @@ const Information = () => {
             </Link>
           </div>
           <div className="Information-next">
-            <Link to="/checkout/payment">
-              Continuar al pago
-            </Link>
+            <button type="button" onClick={handleSubmit}>Proceder al pago</button>
           </div>
         </div>
       </div>
       <div className="Information-sidebar">
         <h3>Pedido:</h3>
-        <div className="Information-item">
-          <div className="Information-element">
-            <h4>ITEM name</h4>
-            <span>$10</span>
+        {cart.map((item) => (
+          <div className="Information-item" key={item.title}>
+            <div className="Information-element">
+              <h4>{item.title}</h4>
+              <span>${item.price}</span>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
